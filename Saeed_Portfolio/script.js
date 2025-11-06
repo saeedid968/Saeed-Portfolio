95f33aabf8011c41b0b632e04bf3a80b19b8fab3
Saeed Ahmed <saeedid968@gmail.com>
2025-11-06 05:22:20 +0500

    Saeed Portfolio HTML, CSS, JS

---
+++ Saeed_Portfolio/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "Mern Stack Developer", "Frontend Specialist"],
+  typeSpeed: 90,
+  backSpeed: 50,
+  loop: true,
+});
+
+const typed2 = new Typed(".typing-2", {
+  strings: ["Web Developer", "Mern Stack Developer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
